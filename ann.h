a09dd1083d6711885d116b1ec58e30529b1fa291
Sindiso Mkhatshwa <mkhsin035@myuct.ac.za>
2020-06-11 00:59:44 +0200

    changed return type of activate function to std::vector

--- ann.h
+++ ann.h
@@ -1,5 +1,3 @@
-
-
 /* 
  * File:   ann.h
  * Author: Sindiso
@@ -9,31 +7,44 @@
 
 #ifndef ANN_H
 #define ANN_H
+//------------------------------------------------------------------------------
+//INCLUDES
+//------------------------------------------------------------------------------
 #include <iostream>
 #include <stdlib.h>     /* srand, rand */
 #include <time.h>       /* time */
 #include <vector>
+
 namespace MKHSIN035{
+    //--------------------------------------------------------------------------
+    //Neuron class interface
+    //--------------------------------------------------------------------------
   class neuron{
   private:
       int output;
-      int n_prev_neurons;
+      int prev_neurons;
       double theta;
       std::vector<double> weights;
   public:
       neuron();
-      neuron(int n_prev_neurons, double theta);
+      neuron(int prev_neurons, double weights[]);
       int activation_fn(const double inputs[]);
       int get_output();
   }; 
+  //----------------------------------------------------------------------------
+  //Layer class interface
+  //----------------------------------------------------------------------------
   class layer{
   private:
+      int neurons;//Number of neurons in this layer
+      int prev_neurons;//Number of previous neurons
       vector<neuron> neurons;//All neurons in this layer
       vector<int> outputs;//outputs from all neurons in this layer
   public:
-            
+      layer();
+      layer(int prev_neurons, int neurons);
       void learn(double input[], double t);//learning fn
-
+      std::vector<int> activate(double inputs[]);
   };
 };
 
