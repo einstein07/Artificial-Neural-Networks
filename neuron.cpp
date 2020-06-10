94410712ed3a77020b9a0b7a7d0ab597450ecef6
Sindiso Mkhatshwa <mkhsin035@myuct.ac.za>
2020-06-11 00:47:48 +0200

    changed initialiser list, also changed threshold value to 0 for the activation fn

--- neuron.cpp
+++ neuron.cpp
@@ -5,16 +5,16 @@
  * Created on 09 June 2020, 02:50
  */
 #include "ann.h"
-
+//------------------------------------------------------------------------------
+//FUNCTION DEFS
+//------------------------------------------------------------------------------
 //Default constructor
-MKHSIN035::neuron::neuron():n_prev_neurons(0), theta(0){}
+MKHSIN035::neuron::neuron():prev_neurons(0){}
 
 //Constructor
-MKHSIN035::neuron::neuron(int n_prev_neurons, double theta):n_prev_neurons(n_prev_neurons), theta(theta){
-    srand (time(NULL));
-    for(int i = 0; i < n_prev_neurons; i++){
-        double w= ((double) rand() / (RAND_MAX));
-        this->weights.push_back(w);
+MKHSIN035::neuron::neuron(int prev_neurons, double weights[]):prev_neurons(prev_neurons){
+    for(int i = 0; i < prev_neurons; i++){
+        this->weights.push_back(weights[i]);
     }
 }
 
@@ -23,27 +23,14 @@ int MKHSIN035::neuron::activation_fn(const double inputs[]){
     for(int i = 0; i < this->n_prev_neurons; i++){
         sum += this->weights[i]*inputs[i];
     }
-    this->output = (sum>theta?1:0);
+    this->output = (sum>0?1:0);
     return output;
 }
       
 int MKHSIN035::neuron::get_output(){return this->output;}
       
-void MKHSIN035::layer::learn(double input[], double t){
-    int y = activation_fn(input);
-            
-    while(y != t){
-        for (int i = 0; i < this->n_prev_neurons; i++){
-                        
-            std::cout<<"It "<<(i+1)<<" Weight: "<<weights[i]<<" ";
-            this->weights[i] = weights[i] + (t-y) * input[i];
-
-        }
-        y = activation_fn(input);
-        std::cout<<std::endl;
-    }
-    std::cout<<weights[0]<<" "<<weights[1]<<std::endl;
-    //return true;
-}
+//------------------------------------------------------------------------------
+//EOF
+//------------------------------------------------------------------------------
 
 
