d66f7d58f91697e61a3638337b21c16a610a1849
Sindiso Mkhatshwa <mkhsin035@myuct.ac.za>
2020-06-13 17:20:54 +0200

    Fixed errors(bugs), added function def for get_n_neurons

--- layer.cpp
+++ layer.cpp
@@ -20,24 +20,24 @@ MKHSIN035::layer::layer(int prev_neurons_n, int neurons):prev_neurons(prev_neuro
     for (int i = 0; i < neurons_n; i++){ 
         for(int j = 0; j < this->prev_neurons; j++){
             rand_w[j]= ((double) rand() / (RAND_MAX));
+            //std::cout<<"rand: "<<rand_w[j]<<std::endl;
         }
         neuron n(prev_neurons_n+1, rand_w);
         this->neurons.push_back(n);
-        std::cout<<"Neuron created"<<std::endl;
     }
     
 }
 std::vector<int> MKHSIN035::layer::activate(std::vector<int> inputs){
     std::vector<int> values;
     if(neurons[0].get_weights().size() != inputs.size()){
-        values.reserve(inputs.size()+1);
-        values[0] = 1;
-        for(int i = 1; i <= inputs.size(); i++){
-            values[i] = inputs[i];
+        values.push_back(1);
+        for(int i = 0; i <= inputs.size(); i++){
+            values.push_back(inputs[i]);
         }
     }else{
         values = inputs;
     }
+    //std::cout<<values[0]<<" "<<values[1]<<" "<<values[2]<<std::endl;
     for(int i = 0; i < neurons_n; i++){
         outputs.push_back(neurons[i].activation_fn(values));
     }
@@ -48,29 +48,30 @@ void MKHSIN035::layer::learn(int i, std::vector<int> inputs, double t){
     if(neurons[0].get_weights().size() != inputs.size()){
                 
         values.push_back(1);
-        for(int i = 1; i <= inputs.size(); i++){
+        for(int i = 0; i <= inputs.size(); i++){
             values.push_back(inputs[i]);
         }
     }else{
         values = inputs;
     }
-    std::cout<<"Biasing added: "<<values.size()<<std::endl;
     std::vector<double> weights = neurons[i].get_weights();
     int y = neurons[i].activation_fn(values);
                 
-    std::cout<<"weights fetched and neuron activated"<<std::endl;
+
 
     while(y != t){
+        //Bias term
         for (int i = 0; i < this->prev_neurons; i++){
-                        
-            std::cout<<"It "<<(i+1)<<" Weight: "<<weights[i]<<" ";
-            weights[i] = weights[i] + (t-y) * values[i];
-
+            weights[i] = weights[i] + 0.001*(t-y) * values[i];
         }
+            
+        neurons[i].set_weights(weights);
+
         y = neurons[i].activation_fn(values);
         //std::cout<<std::endl;
     }
-    neurons[i].set_weights(weights);
     //return true;
 }
 std::vector<double> MKHSIN035::layer::get_weights(int i){return neurons[i].get_weights();}
+      
+int MKHSIN035::layer::get_n_neurons(){return this->neurons.size();};
