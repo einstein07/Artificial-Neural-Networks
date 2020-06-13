33e1ee345e1003a6711186c13f3fed34a32c9fb4
Sindiso Mkhatshwa <mkhsin035@myuct.ac.za>
2020-06-13 17:22:59 +0200

    Added code to test neuron and layer classes. Also implemented code for training perceptrons

--- main.cpp
+++ main.cpp
@@ -14,17 +14,59 @@ using namespace std;
  */
 int main(int argc, char** argv) {
 
-    std::vector<int> x;
-    x.push_back(0);
-    x.push_back(1);
-    double t = 1; 
-    MKHSIN035::layer n(2, 2);
-    n.learn(1, x, 1);
-    std::vector<double> weights = n.get_weights(1);
-       
-    std::cout<<weights[0]<<" "<<weights[1]<<" "<<weights[2]<<std::endl;
-
-    //if(n.learn(x,t))std::cout<<"Done learning: "<<n.get_output()<<endl;
+    vector < vector<int> > data =   {{0, 0},
+                                    {0, 1},
+                                    {1, 0},
+                                    {1, 1}};
+    vector < vector<int> > label =  {{0, 1, 1, 1},//OR gate
+                                    {1, 1, 1, 0}, //NAND gate
+                                    {0, 0, 0, 1}};//AND gate
+    int n_neurons = 3;
+    int prev_neurons = 2;
+    MKHSIN035::layer l(prev_neurons, n_neurons);
+    for(int i = 0; i < l.get_n_neurons(); i++){
+        for(int j = 0; j < label[i].size(); j++){
+            l.learn(i, data[j], label[i][j]);
+        }
+    }
+//    n.learn(0, vc[0], t);
+//    std::vector<double> weights = n.get_weights(0);
+//       
+//    std::cout<<weights[0]<<" "<<weights[1]<<" "<<weights[2]<<std::endl;
+//        
+//    t = 0;
+//    std::vector<int> x1;
+//    //x1.push_back(0.5);
+//    x1.push_back(0);
+//    x1.push_back(1);
+//    n.learn(0, x1, t);
+//    weights = n.get_weights(0);
+//    std::cout<<weights[0]<<" "<<weights[1]<<" "<<weights[2]<<std::endl;
+//    
+//    std::vector<int> x2;
+//    //x2.push_back(0.5);
+//    x2.push_back(1);
+//    x2.push_back(0);
+//    n.learn(0, x2, t);
+//    weights = n.get_weights(0);
+//    std::cout<<weights[0]<<" "<<weights[1]<<" "<<weights[2]<<std::endl;
+//
+//    t = 1;
+//    std::vector<int> x3;
+//    //x3.push_back(0.5);
+//    x3.push_back(1);
+//    x3.push_back(1);
+//    n.learn(0, x3, t);
+//    weights = n.get_weights(0);
+//    std::cout<<weights[0]<<" "<<weights[1]<<" "<<weights[2]<<std::endl;
+//    //std::cout<<"Done learning"<<std::endl;
+//    
+//    //n.learn(0, x, 1);
+//    std::vector<int> tes;
+//    tes.push_back(0);
+//    tes.push_back(1);
+//    x1 = n.activate(tes);
+    std::cout<<"Done learning"<<endl;
     
     return 0;
 }
